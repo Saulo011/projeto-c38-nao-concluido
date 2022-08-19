d86d3bf6adf0a9e02034e8be99d397c526aa8dd7
Saulo011 <107715313+Saulo011@users.noreply.github.com>
2022-08-19 20:43:45 -0300

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,44 @@
+class Contestant {  //professora acho que estou com um erro no banco de dados porque ja tentei varias vezes e nao esta dando certo. (obs:ja tentei de todos os jeitos possiveis)
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var contestantCountRef = database.ref('contestantCount');
+
+
+    contestantCountRef.on("value",(data)=>{
+     contestantCount = data.val();
+     })
+
+    //contestantCountRef.on("value",()=>{
+    //  contestantCount = data.val();
+    //})
+
+   // contestantCountRef.on("value",(data)=>{
+   //   contestantCount = data();
+   // })
+  }
+
+    updateCount(count){
+    //database.ref("/").update({
+    //  contestantCount: count
+    //});
+
+    database.ref("/").update({
+    contestantCount: 0
+    });
+
+    //database.ref("/").update({
+    //  contestantCount: counts
+    //});
+
+    //database("/").update({
+    //  contestantCount = count
+    //});
+   }
+ 
+ 
+}
